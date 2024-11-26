e8fef3257fc744864c25f81d0c587a96ab0fc3ad
许飞 <feixu@zhejianglab.com>
2024-11-26 15:23:27 +0800

    first

--- util/logger.py
+++ util/logger.py
@@ -9,7 +9,7 @@ handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(module)s:%(lineno)s:%(funcName)s - %(message)s")
 handler.setFormatter(formatter)
 
-file_handler = logging.FileHandler('log/access.log')  # 输出到文件 app.log
+file_handler = logging.FileHandler('access.log')  # 输出到文件 app.log
 file_handler.setLevel(logging.DEBUG)  # 如果需要设置文件日志级别，可以这里指定
 file_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(module)s:%(lineno)s:%(funcName)s - %(message)s")
 file_handler.setFormatter(file_formatter)
