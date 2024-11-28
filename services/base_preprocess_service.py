cc27c38afc5337705431497d497b0533d2cab279
许飞 <feixu@zhejianglab.com>
2024-11-28 11:15:15 +0800

    first

--- services/base_preprocess_service.py
+++ services/base_preprocess_service.py
@@ -28,5 +28,8 @@ class PreprocessFactory():
         elif service_name == "SODA-D":
             from services.dior_sodad_preprocess_service import DiorSodaDPreprocessService
             return DiorSodaDPreprocessService(dataset_path=dataset_path)
+        elif service_name == "WHU-RS19":
+            from services.whurs19_preprocess_service import WhuRs19PreprocessService
+            return WhuRs19PreprocessService(dataset_path=dataset_path)
         else:
             raise ValueError("Invalid service name")
