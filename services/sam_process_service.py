61ac4b6d35a65361a71bff1f30b0950c9c4bc689
许飞 <feixu@zhejianglab.com>
2024-11-28 15:32:49 +0800

    first

--- services/sam_process_service.py
+++ services/sam_process_service.py
@@ -62,6 +62,7 @@ class SamProcessService():
         except KeyboardInterrupt:
             logger.warning("Keyboard interrupt detected. Exiting.")
             executor.shutdown(wait=False, cancel_futures=True)
+            raise
 
         return result
     
