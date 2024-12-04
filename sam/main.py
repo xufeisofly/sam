11c3d8ea1cc97350f3394ad19ee3a62212e9888e
许飞 <feixu@zhejianglab.com>
2024-12-04 10:09:24 +0800

    fix

--- sam/main.py
+++ sam/main.py
@@ -70,7 +70,7 @@ def main():
         ori_label_2_id_map=preprocessor.ori_label_2_id_map(), 
         use_gpu=args.use_gpu, 
         parallel_num=args.parallel_num, 
-        gpu_ids=args.gpu_ids.split(','))
+        gpu_ids=[int(gpu_id) for gpu_id in args.gpu_ids.split(',')])
     output_service = OutputService(args.dataset)
     output_service.clear_output()
     process_result_without_masks = processor.get_result_without_mask(preprocess_result, merge_mask=bool(args.merge_mask))
